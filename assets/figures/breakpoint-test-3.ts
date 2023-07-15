4c9a7573cd4e8f99a95631601603827df2d9368c
Kalane <vkaelin@hey.com>
2023-07-15 17:25:46 +0200

    feat: add screenshot of load test visual result

--- assets/figures/breakpoint-test-3.ts
+++ assets/figures/breakpoint-test-3.ts
@@ -1,27 +1,28 @@
 export async function drawMaxRandomPixels(
-	socket: K6SocketIo,
-	CounterPixels: Counter,
-	sleepBetweenPixels = 3,
-	sleepAfter = 30,
+  socket: K6SocketIo,
+  CounterPixels: Counter,
+  sleepBetweenPixels = 3,
+  sleepAfter = 30,
 ) {
-	socket.setEventMessageHandle(ServerEvent.UPDATE_BOARD, () => {});
+  socket.setEventMessageHandle(ServerEvent.UPDATE_BOARD, () => { });
 
-	const { initialState } = await joinRoom(socket);
-	const { canvasSize, colors, maxPixels } = initialState;
+  const protocol = __ENV.PROD === 'true' ? 'https' : 'http';
+  http.get(`${protocol}://${__ENV.FRONTEND}`);
 
-	for (let i = 0; i < maxPixels; i++) {
-		sleep(random(sleepBetweenPixels));
-		CounterPixels.add(1);
-		socket.send(
-			ClientEvent.PIXEL_FROM_PLAYER,
-			randomPixel(canvasSize, colors.length),
-			null,
-		);
-	}
+  const { initialState } = await joinRoom(socket);
+  const { canvasSize, colors, maxPixels } = initialState;
 
-	const protocol = __ENV.PROD === 'true' ? 'https' : 'http';
-	http.get(`${protocol}://${__ENV.FRONTEND}`);
-	sleep(sleepAfter);
+  for (let i = 0; i < maxPixels; i++) {
+    sleep(random(sleepBetweenPixels));
+    CounterPixels.add(1);
+    socket.send(
+      ClientEvent.PIXEL_FROM_PLAYER,
+      randomPixel(canvasSize, colors.length),
+      null,
+    );
+  }
 
-	socket.close();
+  sleep(sleepAfter);
+
+  socket.close();
 }
